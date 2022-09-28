664bb7ccd629fde424f3bbf17c3380213f26ee1e
RicardoAguilar00 <113196350+RicardoAguilar00@users.noreply.github.com>
2022-09-28 10:08:13 -0500

    Add files via upload

---
+++ Calculadora/main.cpp
@@ -0,0 +1,47 @@
+/*
+ * Author: Ricardo Aguilar Hernandez
+ * email: aguilar.hernandez.ricardo@cominidad.unam.mx
+ * 28/09/2022
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a=0;
+    char b;
+    float c=0;
+
+cout<<"Este programa hace operaciones basicas entre 2 numeros"<<endl;
+cout<<"Primer numero: "<<endl;
+cin>>a;
+cout<<"Operacion: "<<endl;
+cin>>b;
+cout<<"Segundo numero: "<<endl;
+cin>>c;
+
+if (b == '+')
+{
+    cout<<"Resultado: \n"<< a << b << c << " = " << a+c;
+}
+else if (b == '-')
+{
+    cout<<"Resultado: \n"<< a << b << c << " = " << a-c;
+}
+else if (b== '*') {
+    cout<<"Resultado: \n"<< a << b << c << " = " << a*c;
+}
+else if (b== '/') {
+    cout<<"Resultado: \n"<< a << b << c << " = " << a/c;
+}
+else if (b== '%') {
+    int A=a;
+    int C=c;
+    cout<<"Resultado (Parte entera) : \n"<< a << b << c << " = " << A%C;
+}
+else {
+    cout << "error de sitaxis\n";
+}
+    return 0;
+}
