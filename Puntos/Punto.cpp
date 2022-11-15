c40f53fd645ba2216bf766fe346627a2413c2d3a
RicardoAguilar00 <113196350+RicardoAguilar00@users.noreply.github.com>
2022-11-15 17:47:13 -0600

    Create Punto.cpp

---
+++ Puntos/Punto.cpp
@@ -0,0 +1,45 @@
+//
+// Author: Ricardo Aguilar Hernández
+// 12/Nov/2021
+//
+#include <iostream>
+
+#include "Punto.h"
+#include <math.h>
+#define PI 3.14159265
+using namespace std;
+
+Punto2D::Punto2D() {
+    a = 0;
+    b = 0;
+    c = 0;
+}
+
+void Punto2D::SetPosicion(float x,float y){
+    a = x;
+    b = y;
+}
+
+void Punto2D::Trasladar(float temp1, float temp2) {
+    a = a + temp1;
+    b = b + temp2;
+}
+
+void Punto2D::RotarRespectoAlOrigen(float temp1){
+    c = a;
+    a = c*cos(temp1*PI/180) - b*sin(temp1*PI/180);
+    b = c*sin(temp1*PI/180) + b*cos(temp1*PI/180);
+}
+
+void Punto2D::Escalar(float temp1, float temp2) {
+    a = a*temp1;
+    b = b*temp2;
+}
+
+float Punto2D::GetX() {
+    return a;
+}
+
+float Punto2D::GetY() {
+    return b;
+}
